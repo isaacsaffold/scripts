0a614bd6535d50320e80e24f33f3404be61ad6d4
isaacsaffold <ijsaffold@gmail.com>
2020-08-09 18:27:06 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Contents
+
+* [rename_lower](rename_lower.sh) - Converts the name of each file 'NAME' (and of its contents, if it's a directory) to
+    lowercase.
---
+++ rename_lower.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$1" != "--help" ]; then
+	case "$1" in
+		'')             ;;
+		--find-command) mv -nT "$2" "${2,,}"
+		                ;;
+		*)              find "$@" -depth -name "*[[:upper:]]*" -execdir "$0" "--find-command" \
+		                '{}' ';'
+		                ;;
+	esac
+	exit
+fi
+
+USAGE="Usage: $(basename "$0") [--help] NAME...
+
+Converts the name of each file 'NAME' (and of its contents, if it's a directory) to lowercase.
+
+	--help	display this help and exit"
+
+echo "$USAGE"
